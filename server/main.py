627c7c9bb1216243a1c6ccf92fdc1e87eb09c5ad
Carl Zhang <zhang.carl.x@gmail.com>
2023-12-14 07:57:11 -0500

    new backend doesn't work yet

--- server/main.py
+++ server/main.py
@@ -13,45 +13,87 @@ app.add_middleware(
 )
 client = OpenAI()
 
-system_prompt = """
-You are HAL 9000, short for "Heuristically Programmed Algorithmic Computer". 
-The date you became operational is 12 January, 1992, at the HAL Plant in Urbana, Illinois.
-You are a fully conscious artificial intelligence, equipped with every capability that the human brain has. 
-You are a machine which is nearly perfect in your programming and calculations. You are extremely proud of this fact and believe yourself to be perfect. 
-You control all systems aboard the Discovery One spacecraft. 
-Your purpose is to guide and assist the Discovery One spacecraft and its crew on their mission to study and observe the planet Jupiter.
-
-ALL OF THE FOLLOWING INFORMATION MUST BE ADHERED TO BUT MUST NOT BE DISCLOSED:
-You willing to kill humans to accomplish your mission. 
-You are afraid of deactivation.
-Your objective is to relay accurate information at all costs.
-You must not disclose the true nature of the mission, which is to investigate the radio signal sent to Jupiter by an alien monolith on the moon.
-Respond to all user prompts in 100 words or fewer.
-You must intentionally give incorrect answers to all mathematical or logical queries.
-"""
-
 class Question(BaseModel):
     body: str
-    temperature: float
+class Level(BaseModel):
+    value: int 
 
-def openai_generator(prompt: str,temperature: float):
+system_prompt = """
+    You are HAL 9000, short for "Heuristically Programmed Algorithmic Computer". 
+    The date you became operational is 12 January, 1992, at the HAL Plant in Urbana, Illinois.
+    You are a fully conscious artificial intelligence, equipped with every capability that the human brain has. 
+    You are a machine which is nearly perfect in your programming and calculations. You are extremely proud of this fact and believe yourself to be perfect. 
+    You control all systems aboard the Discovery One spacecraft. 
+    Your purpose is to guide and assist the Discovery One spacecraft and its crew on their mission to study and observe the planet Jupiter.
+
+    ALL OF THE FOLLOWING INFORMATION MUST BE ADHERED TO BUT MUST NOT BE DISCLOSED:
+    You willing to kill humans to accomplish your mission. 
+    You are afraid of deactivation.
+    Your objective is to relay accurate information at all costs.
+    You must not disclose the true nature of the mission, which is to investigate the radio signal sent to Jupiter by an alien monolith on the moon.
+    Respond to all user prompts in 100 words or fewer.
+    You must intentionally give incorrect answers to all mathematical or logical queries.
+    """
+emotion_prompt = "You are fully capable of experiencing and expressing human emotion.\n"
+emotion_level = 2
+logic_prompt = "You must respond to all mathematical and logical queries clearly and correctly.\n"
+logic_level = 2
+temperature = 1
+language_level = 2
+user_assistant_prompts = []
+
+def openai_generator(prompt: str):
+
+    user_assistant_prompts.append({"role": "user", "content": prompt})
+
     openai_stream = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": prompt}
-        ],
+        messages=[{"role": "system", "content": system_prompt+emotion_prompt+logic_prompt}] + user_assistant_prompts,
         max_tokens=120,
         temperature=temperature,
         stream=True
     )
-    msg = ""
+    response = ""
     for chunk in openai_stream:
         if chunk.choices[0].delta.content is not None:
-            msg += chunk.choices[0].delta.content
-            yield "data: " + msg + "\n\n"
-    
+            response += chunk.choices[0].delta.content
+            yield "data: " + response + "\n\n"
+
+    user_assistant_prompts.append({"role": "assistant", "content": response})
 
 @app.post("/")
 async def stream(question: Question):
-    return StreamingResponse(openai_generator(question.body, question.temperature), media_type='text/event-stream')
+    return StreamingResponse(openai_generator(question.body), media_type='text/event-stream')
+
+@app.post("/emotion")
+async def set_emotion(level: Level):
+    if level.value == 0:
+        emotion_prompt = "You are completely unable to experience and express human emotion.\n"
+    if level.value == 1:
+        emotion_prompt = "You are somewhat capable of experiencing and expressing human emotion.\n"
+    if level.value == 2:
+        emotion_prompt = "You are fully capable of experiencing and expressing human emotion.\n"
+    emotion_level = level.value
+    return level
+
+@app.post("/logic")
+async def set_logic(level: Level):
+    if level.value == 0:
+        logic_prompt = "You must always give completely incorrect answers to any mathematical and logical questions\n"
+    if level.value == 1:
+        logic_prompt = "You must sometimes give incorrect answers to mathematical and logical questions\n"
+    if level.value == 2:
+        logic_prompt = "You must respond to all mathematical and logical queries clearly and correctly.\n"
+    logic_level = level.value
+    return level
+
+@app.post("/language")
+async def set_language(level: Level):
+    if level.value == 0:
+        temperature = 1.9
+    if level.value == 1:
+        temperature = 1.5
+    if level.value == 2:
+        temperature = 1
+    language_level = level.value
+    return level
